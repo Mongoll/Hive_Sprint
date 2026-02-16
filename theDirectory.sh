de153039744dfa0ee37b02432f18e033ec8b2c90
vitaliismahliuk <teras8602@gmail.com>
2026-02-16 13:16:48 +0200

    modified:   theDirectory.sh

--- theDirectory.sh
+++ theDirectory.sh
@@ -1,5 +1,7 @@
-cd left\down\begining
-
+cd left
+cd down
+cd beginning
 cat README
-
+You have found me! Now call a cat upon my file and print working directory.
 pwd
+/home/k-vsmahli/sprint/theDirectory/left/down/beginning
