41f6d70c73638921ad7509fe993dc33d5108c0f9
vitaliismahliuk <teras8602@gmail.com>
2026-02-16 13:52:47 +0200

    modified:   theDirectory.sh

--- theDirectory.sh
+++ theDirectory.sh
@@ -1,2 +1,2 @@
-echo You have found me! Now call a cat upon my file and print working directory.
-echo /app/directoriestheDirectory/left/down/beginning
+echo "You have found me! Now call a cat upon my file and print working directory."
+echo "/app/directoriestheDirectory/left/down/beginning"
