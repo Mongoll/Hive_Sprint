e765e736eea32f30fcc185eac356005f48f4fa0e
Mongoll <teras8602@gmail.com>
2026-02-17 19:08:16 +0200

    new file:   TheLeepYear.go

---
+++ TheLeepYear.go
@@ -0,0 +1,25 @@
+package sprint
+
+/* import (
+    "fmt"
+)
+ */
+// IsLeapYear returns true if the given year is a leap year, false otherwise
+func IsLeapYear(year int) bool {
+    if year%4 != 0 {
+        return false
+    } else if year%100 != 0 {
+        return true
+    } else if year%400 != 0 {
+        return false
+    } else {
+        return true
+    }
+}
+/* 
+func main() {
+    fmt.Println(IsLeapYear(2020)) // true
+    fmt.Println(IsLeapYear(1900)) // false
+    fmt.Println(IsLeapYear(2000)) // true
+    fmt.Println(IsLeapYear(2021)) // false
+} */
