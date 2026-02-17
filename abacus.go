9754cc00b5047ca1adf98d8efcb6fca6dba38ab4
Mongoll <teras8602@gmail.com>
2026-02-17 19:30:50 +0200

    modified:   abacus.go

--- abacus.go
+++ abacus.go
@@ -1,8 +1,15 @@
 package main
 
+import "fmt"
+
 func Abacus(a int, b int) int {
 	if  b == 0 {
 		panic("division by zero")
 	}
 	return a / b
+}
+
+func main() {
+	result := Abacus(10, 2)
+	fmt.Println("Result:", result)
 }
