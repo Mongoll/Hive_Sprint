c23d29e7c07da09324579f8e0a4171a2785c5498
Mongoll <teras8602@gmail.com>
2026-02-17 18:49:40 +0200

    new file:   negative.go

---
+++ negative.go
@@ -0,0 +1,15 @@
+package sprint
+
+// import "fmt"
+
+// IsNegative returns true if n is less than 0, otherwise false.
+func IsNegative(n int) bool {
+    return n < 0
+}
+/* 
+func main() {
+    // Examples
+    fmt.Println(IsNegative(6))  // Output: false
+    fmt.Println(IsNegative(-5)) // Output: true
+    fmt.Println(IsNegative(0))  // Output: false
+} */
