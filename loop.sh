1c371b987f926167202d97c2513170616e5dc6e7
Mongoll <teras8602@gmail.com>
2026-02-17 17:16:33 +0200

    modified:   loop.sh

--- loop.sh
+++ loop.sh
@@ -1,9 +1,9 @@
 count=$1
-if [[ "$count" -gt 100 ]]; then
+if [ "$count" -gt 100 ]; then
   count=100
 fi
 i=1
-while [[ "$i" -le "$count" ]]; do
+while [ "$i" -le "$count" ]; do
   echo "This is loop number $i"
   i=$((i + 1))
 done
