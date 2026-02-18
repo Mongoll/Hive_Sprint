7a8139df431987a96f6ddc56600265196f8115db
vitaliismahliuk <teras8602@gmail.com>
2026-02-18 10:56:48 +0200

    modified:   mean.go

--- mean.go
+++ mean.go
@@ -1,11 +1,11 @@
 package sprint
 
-//import "fmt"
+import "fmt"
 
 func Mean(a, b, c float32) float32 {
-	return (a+b+c)/3.0
+	return (a+b+c)/3.0 //can use just 3, but it's float and better use with '.'
 }
 
-/* func main() {
+func main() {
 	fmt.Println(Mean(5.0, 10.0, 3.5))
-} */
+}
