8a2603b4a816377efc2bd137e054e0a69f12065a
vitaliismahliuk <teras8602@gmail.com>
2026-02-18 11:13:10 +0200

    modified:   reversealphabetvalue.go 	modified:   strconcat.go

--- reversealphabetvalue.go
+++ reversealphabetvalue.go
@@ -4,9 +4,9 @@ package sprint
 
 func ReverseAlphabetValue(ch rune) rune {
 	
-	return 'a' + 'z' - ch
+	return 'a' + 'z' - ch //for example: if ch=a unicode(97) then 97+122-97(ch)=122 -> z
 }
 
-/* func main(){
+func main(){
 	fmt.Println(string(ReverseAlphabetValue('a')))
-} */
+}
