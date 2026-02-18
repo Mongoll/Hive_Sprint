e2a83cb31d6ddd32bbd7c2cebfbb02a5fd7dddcf
vitaliismahliuk <teras8602@gmail.com>
2026-02-18 11:06:01 +0200

    modified:   shiftby.go

--- shiftby.go
+++ shiftby.go
@@ -1,13 +1,13 @@
 package sprint
 
-//import "fmt"
+import "fmt"
 
 func ShiftBy(r rune, step int) rune {
-	runeStep := ((step % 26) + 26) % 26
-	newPosition := int(r-'a'+rune(runeStep)) % 26
-	return 'a' + rune(newPosition)
+	runeStep := ((step % 26) + 26) % 26 // we need positiv argument, move to -1=step forward fo 25 (always from 0 to 25)
+	newPosition := int(r-'a'+rune(runeStep)) % 26 // interpret letter to position -> add step -> move out of alphabet
+	return 'a' + rune(newPosition) // interpret position to letter
 }
 
-/* func main(){
+func main(){
 	fmt.Println(string(ShiftBy('a', 4)))
-} */
+}
