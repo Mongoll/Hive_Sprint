8968405cd5ad88698f7851d48d2a5ad9f6aa7eac
vitaliismahliuk <teras8602@gmail.com>
2026-02-19 12:29:00 +0200

    new file:   strtoInt.go

---
+++ strtoInt.go
@@ -0,0 +1,23 @@
+package sprint
+
+/* import (
+	"fmt"
+) */
+
+func StrToInt(s string) int {
+	num := 0
+
+	for i := 0; i < len(s); i++ {	//loop acording string length
+		if s[i] < '0' || s[i] > '9' { // check If the string is not a valid number
+			return 0
+		}	
+		digit := s[i] - '0'        // convert character to integer
+		num = num*10 + int(digit)  // shift previous digits left and add new
+	}
+
+	return num
+}
+
+/* func main() {
+	fmt.Println(StrToInt("10203"))
+} */
