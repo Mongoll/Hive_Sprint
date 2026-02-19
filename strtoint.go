a03b4b6c852b65add61da9fd097694e15a390b4e
vitaliismahliuk <teras8602@gmail.com>
2026-02-19 12:34:05 +0200

    modified:   strtoint.go

--- strtoint.go
+++ strtoint.go
@@ -7,6 +7,17 @@ package sprint
 func StrToInt(s string) int {
 	num := 0
 
+	sign := 1
+	start := 0
+
+	// handle optional sign
+	if s[0] == '-' {
+		sign = -1
+		start = 1
+	} else if s[0] == '+' {
+		start = 1
+	}
+
 	for i := 0; i < len(s); i++ {	//loop acording string length
 		if s[i] < '0' || s[i] > '9' { // check If the string is not a valid number
 			return 0
