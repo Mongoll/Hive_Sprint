0aae6673ecf34bfab07b8b75ca557710e6b71ece
Mongoll <teras8602@gmail.com>
2026-02-22 19:36:51 +0200

    new file:   generaterange.go

---
+++ generaterange.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func GenerateRange(min, max int) []int {
+	nums := make([]int, 0)
+	if max > min {
+		for i := min; i < max; i++ {
+			nums = append(nums, i)
+		}
+	}
+	return nums
+}
+
+func main() {
+	fmt.Println(GenerateRange(-1, 4))
+
+}
