0789c7d6e5c171228ef660677fd992214a5646ef
Mongoll <teras8602@gmail.com>
2026-02-24 18:28:55 +0200

    new file:   strsplitby.go

---
+++ strsplitby.go
@@ -0,0 +1,24 @@
+package main
+
+func StrSplitBy(s, sep string) []string {
+	var strarr []string
+
+	if len(sep) == 0 {
+		return []string{s}
+	}
+
+	tempstr := ""
+	i := 0
+	for i < len(s) {
+		if i+len(sep) <= len(s) && s[i:i+len(sep)] == sep {
+			strarr = append(strarr, tempstr)
+			tempstr = ""
+			i += len(sep)
+			continue
+		}
+		tempstr += string(s[i])
+		i++
+	}
+	strarr = append(strarr, tempstr)
+	return strarr
+}
