6b4455048d9b2121ab76393c3b15424e203e6fa7
vitaliismahliuk <teras8602@gmail.com>
2026-02-24 15:03:17 +0200

    new file:   tocapitalcase.go

---
+++ tocapitalcase.go
@@ -0,0 +1,24 @@
+package main
+
+func ToCapitalCase(s string) string {
+	runes := []rune(s)
+	newWord := true
+
+	for i, r := range runes {
+		if (r >= 'a' && r <= 'z') || (r >= 'A' && r <= 'Z') || (r >= '0' && r <= '9') {
+			if newWord {
+				if r >= 'a' && r <= 'z' {
+					runes[i] = r - 32
+				}
+				newWord = false
+			} else {
+				if r >= 'A' && r <= 'Z' {
+					runes[i] = r + 32
+				}
+			}
+		} else {
+			newWord = true
+		}
+	}
+	return string(runes)
+}
