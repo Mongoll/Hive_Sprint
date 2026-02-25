086b252b4bd2c9dbbf2cfe06866e73b7f8f50814
vitaliismahliuk <teras8602@gmail.com>
2026-02-25 14:16:22 +0200

    new file:   pointdiff.go

---
+++ pointdiff.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+type Point struct {
+	X    float32
+	Y    float32
+	Text string
+}
+
+func PointDiff(p1, p2 Point) Point {
+	if p1.X > p2.X {
+		return p1
+	} else if p1.X == p2.X {
+		if p1.Y > p2.Y {
+			return p1
+		}
+	}
+	return p2
+}
+
+func main() {
+	fmt.Println(PointDiff(Point{3, 4, "text"}, Point{3, 5, "text"}))
+}
