71ddf437996af199de24ff372ecbcec27a584e4b
vitaliismahliuk <teras8602@gmail.com>
2026-02-25 15:27:36 +0200

    new file:   getcircle.go

---
+++ getcircle.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+type Circle struct {
+	Radius    float32
+	Diameter  float32
+	Area      float32
+	Perimeter float32
+}
+
+func GetCircle(r float32) Circle {
+	pi := float32(3.14)
+	dia := r * 2
+	area := pi * (r * r)
+	perimeter := 2 * pi * r
+	return Circle{Radius: r, Diameter: dia, Area: area, Perimeter: perimeter}
+}
+func main() {
+	fmt.Println(GetCircle(5))
+}
