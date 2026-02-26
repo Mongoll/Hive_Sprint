e12ed37434dc3faf0732d1e97961de41d3101682
vitaliismahliuk <teras8602@gmail.com>
2026-02-26 10:37:20 +0200

    modified:   nbrbase.go

--- nbrbase.go
+++ nbrbase.go
@@ -4,7 +4,7 @@ package main
 
 func NbrBase(n int, base string) string {
 
-	if len(base) < 2 || len(base) >2 && len(base) < 6 || len(base) == 7{
+	if len(base) < 2 || len(base) >2 && len(base) < 6 {
 		return "NV"
 	}
 		
