26f336a6755d3f26f57bc62a3641cccfa0b9c249
vitaliismahliuk <teras8602@gmail.com>
2026-02-26 12:39:34 +0200

    modified:   factorialiterative.go

--- factorialiterative.go
+++ factorialiterative.go
@@ -3,14 +3,10 @@ package main
 import "fmt"
 
 func FactorialIterative(n int) int {
-	var f int
-	f = 1
-	if n>0{
-		for i := 1; i <= n; i++ {
-			f *= i
-		}
+	if n == 0 || n == 1 {
+		return 1
 	}
-	return f
+	return n * FactorialIterative(n-1)
 }
 
 func main() {
