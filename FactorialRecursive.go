49b92cb16fbab4023d41ee03f0f6a2e145b6feac
vitaliismahliuk <teras8602@gmail.com>
2026-02-26 12:41:09 +0200

    new file:   FactorialRecursive.go

---
+++ FactorialRecursive.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func FactorialIterative(n int) int {
+	if n == 0 || n == 1 {
+		return 1
+	}
+	return n * FactorialIterative(n-1)
+}
+
+func main() {
+	fmt.Println(FactorialIterative(5))
+}
