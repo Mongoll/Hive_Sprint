ab2e009329b071c5c605334d254ce705bd3f680c
vitaliismahliuk <teras8602@gmail.com>
2026-03-02 09:18:50 +0200

    modified:   tothepowerrecursive.go

--- tothepowerrecursive.go
+++ tothepowerrecursive.go
@@ -2,15 +2,15 @@ package main
 /* 
 import "fmt" */
 
-func ToThePowerIterative(n int, power int) int {
+func ToThePowerRecursive(n int, power int) int {
 	if power == 0 {
 		return 1
 	} else if power > 0 {
-		return n * ToThePowerIterative(n, power-1)
+		return n * ToThePowerRecursive(n, power-1)
 	}
 	return 0
 
 }
 /* func main() {
-	fmt.Println(ToThePowerIterative(2, 10))
+	fmt.Println(ToThePowerRecursive(2, 10))
 } */
